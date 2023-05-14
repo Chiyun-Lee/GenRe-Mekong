f08c35d57868f139aa611e713e6bf06241fc065b
Chiyun-Lee <chiyun.alberto.lee@gmail.com>
2023-05-14 10:25:30 +0100

    final commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Analysis of GenRe-Mekong dataset
+
+Python 3.11.3
+
+Please request final .html output from me directly
+
+### Repo organisation
+
+- src (contains utils.py and will be the location where the GenRe-Mekong will be downloaded to)
+
+- .gitignore
+
+- main.ipynb (main analysis. first cell in notebook will download the dataset)
+
+- requirements.txt
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,9 @@ PLOTLY_CONFIG = {"displaylogo": False, "modeBarButtonsToRemove":
                  ["zoom", "pan", "zoomin", "zoomout", "lasso", "pan", "select2d", "resetscale", "toImage"]}
 
 def load_df():
+    """
+    Loads the data from local .xlsx files and does some very simple cleaning
+    """
     df = pd.read_excel("src/20200705-GenRe-PfMasterData-0.39.xlsx", sheet_name = "GenRe-Mekong", engine = "openpyxl")
 
     df = df.loc[
@@ -19,6 +22,9 @@ def load_df():
     return df
 
 def find_location_details(s):
+    """
+    Uses geopy package to query an API to return longitude, latitude and country information of District/City names
+    """
     if "geolocator" not in locals():
         geolocator = Nominatim(user_agent = "MyApp")
     
@@ -32,6 +38,9 @@ def find_location_details(s):
 
 
 def generate_sparsity_plots(df, n_bins = 10):
+    """
+    Plots for analysing the prevalence of "X"s in each row and each column
+    """
     fig, axes = plt.subplots(2, 2, figsize = (7, 3))
 
     axes[0][0].sharex(axes[1][0])
@@ -55,6 +64,7 @@ def generate_sparsity_plots(df, n_bins = 10):
     plt.show()
 
 base_encoder = lambda x: {
+    # Quick and dirty function for pseudo-one-hot-encoding the sequence info
     "A": np.array([1.0, 0.0, 0.0, 0.0]),
     "T": np.array([0.0, 1.0, 0.0, 0.0]),
     "C": np.array([0.0, 0.0, 1.0, 0.0]),
@@ -63,6 +73,10 @@ base_encoder = lambda x: {
     "N": np.array([0.7, 0.7, 0.7, 0.7])}[x]
 
 def optimal_dbscan_fit(encoded_df, min_samples = 5, test_eps_values = np.arange(0.3, 10, 0.2)):
+    """
+    A function that takes a list of epsilon values to try fitting a DBSCAN clustering algorithm with.
+    Provides a dataframe of different metrics for each epsilon value to guide the selection of the optimal hyperparameter. 
+    """
     test_eps_columns = [f"eps={i:.01f}" for i in test_eps_values]
     genotype_columns = encoded_df.columns
     l_labels = []
@@ -76,6 +90,7 @@ def optimal_dbscan_fit(encoded_df, min_samples = 5, test_eps_values = np.arange(
     eps_df.columns = test_eps_columns
     eps_df = pd.concat([encoded_df.reset_index(), eps_df], axis = 1).set_index("SampleId")
     
+    # metrics described in more detail in main.ipynb
     l_percentage_minus_1 = []
     l_n_clusters = []
     l_errors = []
@@ -107,6 +122,10 @@ def optimal_dbscan_fit(encoded_df, min_samples = 5, test_eps_values = np.arange(
 
 
 def spiral_offset(k, r_scale = 0.1, theta_scale = 0.6):
+    """
+    Mathematical function that helps distribute points in a spiral. 
+    Helpful for when multiple scatter points are stacked directly on top of each other to help with visibility. 
+    """
     offsets = []
     for i in range(k):
         theta = i * np.pi / 2
@@ -118,6 +137,9 @@ def spiral_offset(k, r_scale = 0.1, theta_scale = 0.6):
 
 
 def generate_cluster_temporal_df(pca_df):
+    """
+    Cleaning function to overcome the quirks of plotly. Generates the data needed for plotting functions. 
+    """
     fig_df = pca_df.groupby(["Year", "cluster", "AdmDiv1"]).apply(lambda x: pd.Series({"Count": len(x),
                                                                                        "Longitude": x.longitude.values[0],
                                                                                        "Latitude": x.latitude.values[0],
@@ -126,17 +148,20 @@ def generate_cluster_temporal_df(pca_df):
                                                                                        "cluster_str": x.cluster_str.values[0],
                                                                                        })).reset_index()
 
+    # the following steps involving the "buffer" are in place to overcome the quirks of plotly.
+    # without these steps, the legend interacts strangely with the slider
     buffer = [pd.DataFrame({"Year": Year,
                             "cluster": cluster,
                             "cluster_str": f"Cluster {cluster}",
                             "Count": 0,
-                            "Longitude": 0,
-                            "Latitude": 90,
+                            "Longitude": np.nan,
+                            "Latitude": np.nan,
                             "District": 0,
                             "Country": 0}, index = [0]) for cluster in fig_df.cluster.unique() for Year in fig_df.Year.unique()]
     fig_df = pd.concat([fig_df] + buffer).sort_values(["Year", "cluster"]).reset_index(drop = True)
     fig_df["Size"] = fig_df.Count.apply(lambda x: 3 + 5*np.log(x+1))
 
+    # the following steps helps to redistribute datapoints of the same city and spreads them out a bit for visibility
     jitter_df = fig_df.groupby(["Year", "District"]).apply(lambda x: pd.Series({"cluster": list(x.cluster),
                                                                             "idx": list(x.index),
                                                                             "Longitude": np.array(x.Longitude),
